6d50137fe4d9d0d729062980ff33a32068cf78af
MC3D <m.chapman.home@gmail.com>
2019-10-13 18:02:44 -0400

    removes comments for function views

--- polls/views.py
+++ polls/views.py
@@ -6,13 +6,6 @@ from django.views import generic
 from .models import Question, Choice
 from .forms import QuestionForm
 
-# def index(request):
-#     question_list = Question.objects.all()
-#     context = {
-#         'question_list': question_list
-#     }
-#     return render(request, 'polls/index.html', context)
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     # instead of using object_list
@@ -22,38 +15,18 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.all()
 
-# def detail(request, question_id):
-#     # question = Question.objects.get(pk=question_id)
-#     question = get_object_or_404(Question, pk=question_id)
-#     context = {'question': question}
-#     return render(request, 'polls/detail.html', context)
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
-# def results(request, question_id):
-#     # return HttpResponse("Results for question #%s" % question_id)
-#     question = get_object_or_404(Question, pk=question_id)
-#     # print(question.choice_set.all())
-#     return render(request, 'polls/results.html', {'question': question})
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-# def create_view(request):
-#     form = QuestionForm()
-#     return render(request, 'polls/create.html', {'form': form})
-
 class CreateView(generic.CreateView):
     model = Question
     fields = '__all__'
     template_name = 'polls/create.html'
-
-# class DeleteView(generic.DeleteView):
-#     model = Question
-#     success_url = reverse_lazy('polls:index')
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
